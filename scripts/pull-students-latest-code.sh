fdffde6af608b5a3de804cda1379935094d074a8
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-03 16:16:05 +0530

    Revert "Update pull-students-latest-code.sh"
    This reverts commit 6940b63727fd6ddb519a132208ed692decbaacc8.

--- scripts/pull-students-latest-code.sh
+++ scripts/pull-students-latest-code.sh
@@ -2,8 +2,6 @@ set -e -o pipefail
 
 function main() {
   pwd
-  mkdir -p submissions
-  cd submissions
   rm -rf "$ASSIGNMENT_NAME-submissions"
   gh classroom clone student-repos -a $ASSIGNMENT_ID
   repos=$(ls $ASSIGNMENT_NAME-submissions)
