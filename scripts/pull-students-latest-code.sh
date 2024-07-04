53870d79fd8f2b343ae30173fa37b1eebf8c9308
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-04 15:09:14 +0530

    Add index.html inside submissions

--- scripts/pull-students-latest-code.sh
+++ scripts/pull-students-latest-code.sh
@@ -3,7 +3,6 @@ set -e -o pipefail
 function main() {
   pwd
   rm -rf .git
-  mkdir -p "submissions"
   cd submissions
   rm -rf "$ASSIGNMENT_NAME-submissions"
   gh classroom clone student-repos -a $ASSIGNMENT_ID
