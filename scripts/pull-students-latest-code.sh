3f87354aabb98da90b389b667b290612af4c0191
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-04 14:11:18 +0530

    Add pipeline to pull students repos and upload

--- scripts/pull-students-latest-code.sh
+++ scripts/pull-students-latest-code.sh
@@ -2,17 +2,15 @@ set -e -o pipefail
 
 function main() {
   pwd
+  rm -rf .git
+  mkdir -p "submissions"
+  cd submissions
   rm -rf "$ASSIGNMENT_NAME-submissions"
   gh classroom clone student-repos -a $ASSIGNMENT_ID
   repos=$(ls $ASSIGNMENT_NAME-submissions)
   for repo in $repos; do
     rm -rf $repo/.git
   done
-  git config --global user.name "polling-students-repos"
-  git config --global user.email "polling-repos@gmail.com"
-  git add .
-  git commit -m "Pulling latest code from students"
-  git push
 }
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
