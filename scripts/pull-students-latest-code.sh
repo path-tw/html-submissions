0ccf5e6a466c8fd04d84971e6030600df14ab252
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-04 14:30:08 +0530

    Testing

--- scripts/pull-students-latest-code.sh
+++ scripts/pull-students-latest-code.sh
@@ -6,12 +6,17 @@ function main() {
   mkdir -p "submissions"
   cd submissions
   rm -rf "$ASSIGNMENT_NAME-submissions"
-  gh classroom clone student-repos -a $ASSIGNMENT_ID
-  repos=$(ls $ASSIGNMENT_NAME-submissions)
-  for repo in $repos; do
-    echo "Removing git from $repo"
-    rm -rf $repo/.git
-  done
+#  gh classroom clone student-repos -a $ASSIGNMENT_ID
+#  repos=$(ls $ASSIGNMENT_NAME-submissions)
+#  for repo in $repos; do
+#    echo "Removing git from $repo"
+#    rm -rf $repo/.git
+#  done
+  cd "$ASSIGNMENT_NAME-submissions"
+  gh repo clone path-tw/my-cart-naveen-kumar-vadla
+  cd my-cart-naveen-kumar-vadla
+  rm -rf .git
+  ls -la
 }
 
 PROJECT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )/../"
