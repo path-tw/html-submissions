310607281c5f836644d5ef08b93bfe5833d36fae
Naveen Kumar Vadla <58026402+naveen-kumar-vadla@users.noreply.github.com>
2024-07-04 15:50:51 +0530

    Generate index.html with students list

--- scripts/pull-students-latest-code.sh
+++ scripts/pull-students-latest-code.sh
@@ -1,8 +1,31 @@
 set -e -o pipefail
+htmlStart="<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <title>Submissions</title>
+    <style>
+      li {
+        margin: 10px;
+      }
 
+      a {
+        text-decoration: none;
+        color: #000;
+      }
+
+      a:hover {
+        text-decoration: underline #f00;
+      }
+    </style>
+  </head>
+  <body>
+    <ol>"
+htmlEnd="</ol></body></html>"
 function pullAssignmentRepos() {
   ASSIGNMENT_NAME="$1"
   ASSIGNMENT_ID="$2"
+  studentsList=""
 
   pushd submissions
   rm -rf "$ASSIGNMENT_NAME-submissions"
@@ -11,8 +34,11 @@ function pullAssignmentRepos() {
   repos=$(ls $ASSIGNMENT_NAME-submissions)
   for repo in $repos; do
     echo "Removing git from $repo"
+    studentsList+="<li><a href=\"./${repo}\">${repo}</a></li>"
     rm -rf $repo/.git
   done
+  code=$htmlStart+$studentsList+$htmlEnd
+  echo $code > index.html
   popd
 }
 
